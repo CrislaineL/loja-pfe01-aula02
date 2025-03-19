6ee9c50b991640fff95a9ea4b9d9f2a591485af7
CrislaineL <crislaineleopoldocandido@gmail.com>
2025-03-19 16:52:11 -0300

    cards estilizados

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('oi');
+const uri = './assets/dados.json';
+var produtos = [];
+fetch(uri)
+.then(resp => resp.json())
+.then(resp => {produtos = resp;})
+.then(() => MostrarCards())
+
+function MostrarCards(){
+    const main = document.querySelector('main');
+    produtos.forEach(p => {
+        main.innerHTML += ` 
+        <div class="card">
+    <h3>${p.produto}</h3>
+    <img src="${p.imagem}" alt="${p.produto}">
+    <p>Preço: ${p.preco}</p> 
+    <p>Descrição: ${p.descricao}</p>
+    <button>Mais Informações</button>
+</div>
+`;
+    })
+
+}
